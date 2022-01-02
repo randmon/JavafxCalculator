cb20867fb26f7de6140e8cf3fc1899c343151727
randmon <ccmarques98@gmail.com>
2022-01-02 22:05:23 +0100

    div 0

--- src/model/domain/operations/Division.java
+++ src/model/domain/operations/Division.java
@@ -3,7 +3,7 @@ package model.domain.operations;
 public class Division implements Operation {
     @Override
     public String calculate(double x, double y) {
-        if (y == 0) return "0";
+        if (y == 0) return "ERROR";
         return formatDouble(x / y);
     }
 
